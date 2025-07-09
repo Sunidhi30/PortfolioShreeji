e2209cfa603958989f99bf80f85df605b59fe624
Sunidhi30 <sunidhi1408.be21@chitkara.edu.in>
2025-07-09 13:22:19 +0530

    Fix: Install Git LFS in Render build

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install Git LFS (Render doesn’t have it pre-installed)
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs -y
+
 # Pull LFS files
 git lfs install
 git lfs pull
