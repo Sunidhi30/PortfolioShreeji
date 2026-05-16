ff40d85ffa59dda24fb240322f7035cbe93eca1b
Sunidhi30 <sunidhiratra21@gmail.com>
2026-05-16 21:39:28 +0530

    add render build script

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,19 @@
+# #!/usr/bin/env bash
+
+# # Install Git LFS (Render doesn’t have it pre-installed)
+# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+# apt-get install git-lfs -y
+
+# # Pull LFS files
+# git lfs install
+# git lfs pull
+
+# # Install dependencies and build
+# npm install
+# npm run build
 #!/usr/bin/env bash
 
-# Install Git LFS (Render doesn’t have it pre-installed)
+# Install Git LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install git-lfs -y
 
@@ -8,6 +21,8 @@ apt-get install git-lfs -y
 git lfs install
 git lfs pull
 
-# Install dependencies and build
+# Install dependencies
 npm install
+
+# Build project
 npm run build
